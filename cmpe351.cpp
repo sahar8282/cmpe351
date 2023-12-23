bd69c40b1d5f8d3c9b2861fba02ac9d669434c11
sahar8282 <s.ataie1321@gmail.com>
2023-12-23 19:54:52 +0200

    menu added

--- cmpe351.cpp
+++ cmpe351.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!";
+    cout <<"CPU Scheduler Simulator"<<endl;
+    cout<<"1) Scheduling Method (None)"<<endl<<"2) Preemptive Mode (Off)"<<endl<<"3) Show Result"<<endl<<"4) End Program"<<endl;
     return 0;
 }
